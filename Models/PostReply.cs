47e0659e450916489b25732e3d0575b7e8cb7630
Sjors Pol <sjors.pol@outlook.com>
2019-11-12 14:41:23 +0100

    setup project and models

---
+++ Models/PostReply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class PostReply
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
